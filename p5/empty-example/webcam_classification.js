48aa669952e66f0e96a834fa1a9c6c7e712caf5b
Sai Siddartha Maram <msaisiddartha1@gmail.com>
2018-10-17 09:58:02 +0530

    webcam

---
+++ p5/empty-example/webcam_classification.js
@@ -0,0 +1,38 @@
+let mobilenet;
+let video;
+
+function modelReady() {
+  console.log('Model is ready!!!');
+  mobilenet.predict(gotResults);
+}
+
+function gotResults(error, results) {
+  if (error) {
+    console.error(error);
+  } else {
+    console.log(results);
+    let label = results[0].className;
+    let prob = results[0].probability;
+    fill(0);
+    textSize(64);
+    text(label, 10, height - 100);
+    mobilenet.predict(gotResults);
+  }
+}
+
+// function imageReady() {
+//   image(puffin, 0, 0, width, height);
+// }
+
+function setup() {
+  createCanvas(640, 480);
+  video = createCapture(VIDEO);
+  video.hide();
+
+  background(0);
+  mobilenet = ml5.imageClassifier('MobileNet',video,modelReady);
+}
+function draw()
+{
+  image(video,0,0);
+}
